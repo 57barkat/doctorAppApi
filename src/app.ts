09b237765915cb29aa2c1ec0a78ea4c8a0365ff3
57barkat <barkatullahkhan120125@gmail.com>
2025-11-09 23:28:35 +0500

    "Fix CORS for frontend"

--- src/app.ts
+++ src/app.ts
@@ -1,39 +1,46 @@
-import express, { Application, NextFunction, Request, Response } from 'express';
-import cors from 'cors';
-import CookieParser from 'cookie-parser';
-import httpStatus from 'http-status';
-import ApiError from './errors/apiError';
-import router from './app/routes';
-import config from './config';
+import express, { Application, NextFunction, Request, Response } from "express";
+import cors from "cors";
+import CookieParser from "cookie-parser";
+import httpStatus from "http-status";
+import ApiError from "./errors/apiError";
+import router from "./app/routes";
+import config from "./config";
 
 const app: Application = express();
 
-app.use(cors());
-app.use(CookieParser());
+// ✅ Allow requests from your frontend
+app.use(
+  cors({
+    origin: ["https://doctor-appfrontend.vercel.app"],
+    credentials: true,
+  })
+);
 
+app.use(CookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.get('/favicon.ico', (req: Request, res: Response) => {
-    res.status(204).end();
-})
+app.get("/favicon.ico", (req: Request, res: Response) => {
+  res.status(204).end();
+});
 
-app.get('/', (req: Request, res: Response) => {
-    res.send(config.clientUrl)
-})
+app.get("/", (req: Request, res: Response) => {
+  res.send(config.clientUrl);
+});
 
-app.use('/api/v1', router);
+app.use("/api/v1", router);
+
 app.use((err: any, req: Request, res: Response, next: NextFunction) => {
-    if (err instanceof ApiError) {
-        res.status(err.statusCode).json({ success: false, message: err.message })
-    } else {
-        res.status(httpStatus.NOT_FOUND).json({
-            success: false,
-            message: 'Something Went Wrong',
-        });
-    }
-    next();
-})
+  if (err instanceof ApiError) {
+    res.status(err.statusCode).json({ success: false, message: err.message });
+  } else {
+    res.status(httpStatus.NOT_FOUND).json({
+      success: false,
+      message: "Something Went Wrong",
+    });
+  }
+  next();
+});
 
 export default app;
